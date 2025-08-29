42ceab59450a25fac867519afc13c04b952e3c94
khurram <khurram_mushtaq55@yahoo.com>
2025-08-29 18:19:00 +0500

    Ads manager added

---
+++ app/src/main/java/com/mmushtaq/orm/allinone/ads/AdsSetting.kt
@@ -0,0 +1,6 @@
+package com.mmushtaq.orm.allinone.ads
+
+object AdsSetting {
+
+    const val USE_TEST_ADS: Boolean = true
+}
--- app/src/main/java/com/mmushtaq/orm/allinone/features/torch/TorchScreen.kt
+++ app/src/main/java/com/mmushtaq/orm/allinone/features/torch/TorchScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +36,8 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mmushtaq.orm.allinone.R
+import com.mmushtaq.orm.allinone.ads.BannerAd
 import com.mmushtaq.orm.allinone.core.widgets.hasCameraPermission
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -68,7 +71,12 @@ fun TorchScreen(vm: TorchViewModel = viewModel()) {
             vm.setTorch(false)
         }
     }
-    Scaffold(topBar = { CenterAlignedTopAppBar(title = { Text("Flashlight") }) }) { pad ->
+    Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = { Text("Flashlight") }) },
+        bottomBar = {
+            BannerAd(
+            )
+        }) { pad ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
